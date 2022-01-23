479e34df10be793cc035e3f41e44d98479a616e2
Pacal2 <i-jackie@hotmail.com>
2022-01-23 23:21:28 +0100

    Controlled Form

--- front-end/src/components/Comic.js
+++ front-end/src/components/Comic.js
@@ -1,19 +1,46 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
+import React, { useEffect, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@mui/material/TextField';
+import { Container ,Paper,Button} from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+     
+    },
+  },
+}));
 
 export default function Comic() {
+  const paperStyle={padding: '50px 20px', width:600, margin: "20px auto"}
+  const[title, setTitle]=useState('');
+  const[rating, setRating]=useState('');
+  const classes = useStyles();
+
   return (
-    <Box
-      component="form"
-      sx={{
-        '& > :not(style)': { m: 1},
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      <TextField id="outlined-basic" label="Comic book title" variant="outlined" />
-      <TextField id="outlined-basic" label="Your rating" variant="outlined" />
-    </Box>
+    <Container>
+      <Paper elevation={3} style={paperStyle}>
+      <h1 style={{color:"blue"}}><u>Add Comic Book</u></h1>    
+    <form className={classes.root} noValidate autoComplete='off'>
+    
+      <TextField id="outlined-basic" 
+      label="Comic book title" 
+      variant="outlined" 
+      fullWidth 
+      value={title}
+      onChange={(e)=>setTitle(e.target.value)} 
+      />
+      <TextField id="outlined-basic" 
+      label="Your rating" 
+      variant="outlined" 
+      fullWidth 
+      value={rating}
+      onChange={(e)=>setRating(e.target.value)} 
+      />
+    
+    </form>
+    </Paper>
+    </Container>
   );
 }
