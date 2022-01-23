ab285493160afb23c5c9ef795e4edc1d3a9fced5
Pacal2 <i-jackie@hotmail.com>
2022-01-23 20:45:13 +0100

    Initial setup

---
+++ src/main/java/com/wseispringproject/wseispringproject/WseiSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.wseispringproject.wseispringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WseiSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WseiSpringProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wseispringproject/wseispringproject/WseiSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wseispringproject.wseispringproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WseiSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
